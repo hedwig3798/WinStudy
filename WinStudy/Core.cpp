1fef3d41e266f96e6ab4f0b9369fbe2f72d85505
hedwig <joey3798@naver.com>
2023-05-10 09:48:23 +0900

    Double Buffering

--- WinStudy/Core.cpp
+++ WinStudy/Core.cpp
@@ -2,20 +2,23 @@
 Core::Core()
 	: m_hWnd(0),
 	m_hDC(0),
-	m_obj{}
+	m_obj{},
+	m_screenSize{}, 
+	m_hBitmap(0),
+	m_memDC(0)
 {
 
 }
 
 Core::~Core()
 {
-
+	ReleaseDC(m_hWnd, m_hDC);
+	DeleteObject(m_memDC);
+	DeleteObject(m_hBitmap);
 }
 
 void Core::Update()
 {
-	
-
 	if (GetAsyncKeyState(VK_LEFT) & 0x8000) 
 	{
 		vec2 nowPos = m_obj.GetPos();
@@ -41,22 +44,35 @@ void Core::Update()
 
 void Core::Rendering()
 {
+	Rectangle(m_memDC, -1, -1, m_screenSize.x + 1, m_screenSize.y + 1);
+
 	RECT rt = m_obj.GetRect();
-	Rectangle(m_hDC, rt.left, rt.top, rt.right, rt.bottom);
+	Rectangle(m_memDC, rt.left, rt.top, rt.right, rt.bottom);
+	
+	BitBlt(m_hDC, 0, 0, m_screenSize.x, m_screenSize.y, m_memDC, 0, 0, SRCCOPY);
+
 }
 
-void Core::Init(HWND hWnd, POINT size)
+void Core::Init(HWND _hWnd, POINT _size)
 {
-	m_hWnd = hWnd;
+	m_hWnd = _hWnd;
+	m_screenSize = _size;
+
 
 	// 창 사각형 크기 조정
-	RECT rt = { 0, 0, size.x, size.y };
+	RECT rt = { 0, 0, _size.x, _size.y };
 	AdjustWindowRect(&rt, WS_OVERLAPPEDWINDOW, FALSE);
 
 	SetWindowPos(m_hWnd, NULL, 100, 100, rt.right - rt.left, rt.bottom - rt.top, 0);
 	m_obj = Object(vec2{ (int)(rt.right - rt.left) / 2, (int)(rt.bottom - rt.top) / 2 }, vec2{ 100, 100 });
 
-	m_hDC = GetDC(hWnd);
+	m_hDC = GetDC(_hWnd);
+
+	m_hBitmap = CreateCompatibleBitmap(m_hDC, m_screenSize.x, m_screenSize.y);
+	m_memDC = CreateCompatibleDC(m_hDC);
+
+	HBITMAP hOldBitmap = (HBITMAP)SelectObject(m_memDC, m_hBitmap);
+	DeleteObject(hOldBitmap);
 
 	// 매니저 초기화
 	TimeMg::GetInst()->Init();
--- WinStudy/Core.h
+++ WinStudy/Core.h
@@ -4,13 +4,10 @@
 #include "TimeMg.h"
 class Core
 {
-	// 싱글턴
 	SINGLE(Core)
 
 public:
-	// 초기화
 	void Init(HWND _hWnd, POINT _size);
-	// 게임 진행
 	void Progress();
 private:
 	void Update();
@@ -20,4 +17,8 @@ private:
 	HWND m_hWnd;
 	HDC m_hDC;
 	Object m_obj;
+	POINT m_screenSize;
+
+	HBITMAP m_hBitmap;
+	HDC m_memDC;
 };
